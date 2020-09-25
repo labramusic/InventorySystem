079f94ce2610e647ce276587dfb7f419ef6708b7
labramusic <labramusiq@gmail.com>
2020-09-25 23:10:41 +0200

    Add camera follow and complete feature 01

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+
+    private Vector3 _positionOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _positionOffset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + _positionOffset;
+    }
+}
